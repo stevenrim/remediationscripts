f9a803295cc3eeeaf554beb7e1662c233e9c7a75
Steven | 9purp0s3 <stevenrim@proton.me>
2025-04-19 08:56:05 -0400

    Update __remediation_template(WN10-AU-000500).ps1

--- __remediation_template(WN10-AU-000500).ps1
+++ __remediation_template(WN10-AU-000500).ps1
@@ -6,15 +6,15 @@ I configured the Application event log size to 32,768 KB (32 MB) or greater. Thi
     Author          : Steven Rim
     LinkedIn        : linkedin.com/in/stevenrim/
     GitHub          : github.com/stevenrim
-    Date Created    : 2024-02-16
-    Last Modified   : 2024-02-16
+    Date Created    : 2025-02-16
+    Last Modified   : 2025-02-16
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN10-AU-000500
 
 .TESTED ON
-    Date(s) Tested  : 2024-02-16
+    Date(s) Tested  : 2025-02-16
     Tested By       : Steven Rim
     Systems Tested  : Windows 10 Pro VM 
     PowerShell Ver. : PowerShell Version: 5.1.19041.5486
